8255e26328bea82ddc4b491bb884ba0574e81573
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-04-04 11:54:01 +0900

    playwright環境を追加

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <nav>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </nav>
+  );
+}
