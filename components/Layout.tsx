8894817e72f97021a39b839aa88986ac9877ca54
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-04-05 22:35:16 +0900

    Layoutコンポーネント作成

---
+++ components/Layout.tsx
@@ -0,0 +1,52 @@
+import Heading from '@/components/heading/Heading';
+import BorderCircle from '@/components/images/BorderCircle';
+import Head from 'next/head';
+import { FC } from 'react';
+import styled from 'styled-components';
+
+const name = 'HaL Code';
+export const siteTitle = 'Next.js blog';
+
+// .headerHomeImage {
+//   width: 8rem;
+//   height: 8rem;
+// }
+
+// .backToHome {
+//   margin: 3rem 0 0;
+// }
+
+const SContainer = styled.div`
+  max-width: 1244px;
+  padding: 0 1rem;
+  margin: 3rem auto 6rem;
+`;
+
+const SHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SBorderCircle = styled(BorderCircle)`
+  width: 6rem;
+  height: 6rem;
+`;
+
+const Layout: FC = ({ children }) => {
+  return (
+    <SContainer>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SHeader>
+        {/* <img src="/images/profile.png" alt="プロフィール画像" /> */}
+        <SBorderCircle src="/images/profile.png" alt="プロフィール画像" />
+        <Heading as='h1' size='lg'>{name}</Heading>
+      </SHeader>
+      <main>{children}</main>
+    </SContainer>
+  );
+};
+
+export default Layout;
