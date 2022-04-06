39255a030e73d6b3f09c99fcfbc914374e31b168
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-04-06 22:35:05 +0900

    リファクタリング

--- components/styles/Heading.tsx
+++ components/styles/Heading.tsx
@@ -1,48 +1,49 @@
 import { FC } from 'react';
-import styled from 'styled-components';
+import styled, { StyledComponent } from 'styled-components';
 
 export type HeadingProps = {
   as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   size: 'md' | 'lg' | 'xl' | '2xl';
 };
 
+export const SDiv: {
+  [key in HeadingProps['size']]: StyledComponent<'div', any, {}, never>;
+} = {
+  md: styled.div`
+    font-size: 1.2rem;
+    line-height: 1.5;
+    text-align: center;
+  `,
+  lg: styled.div`
+    font-size: 2rem;
+    line-height: 1.4;
+    margin-top: 0;
+    font-weight: 500;
+  `,
+  xl: styled.div`
+    font-size: 2rem;
+    line-height: 1.3;
+    font-weight: 800;
+    letter-spacing: -0.05rem;
+    margin: 1rem 0;
+  `,
+  '2xl': styled.div`
+    font-size: 2.5rem;
+    line-height: 1.2;
+    font-weight: 800;
+    letter-spacing: -0.05rem;
+    margin-top: 1rem;
+    margin-bottom: 0;
+  `,
+};
+
 const Heading: FC<HeadingProps> = ({ as, size, children }) => {
-  const SDiv =
-    size === 'md'
-      ? styled.div`
-          font-size: 1.2rem;
-          line-height: 1.5;
-          text-align: center;
-        `
-      : size === 'lg'
-      ? styled.div`
-          font-size: 2rem;
-          line-height: 1.4;
-          margin-top: 0;
-          font-weight: 500;
-        `
-      : size === 'xl'
-      ? styled.div`
-          font-size: 2rem;
-          line-height: 1.3;
-          font-weight: 800;
-          letter-spacing: -0.05rem;
-          margin: 1rem 0;
-        `
-      : // size === '2xl'
-        styled.div`
-          font-size: 2.5rem;
-          line-height: 1.2;
-          font-weight: 800;
-          letter-spacing: -0.05rem;
-          margin-top: 1rem;
-          margin-bottom: 0;
-        `;
+  const SHeader = SDiv[size];
 
   return (
-    <SDiv role="heading" aria-level={Number(as.replace('h', ''))}>
+    <SHeader role="heading" aria-level={Number(as.replace('h', ''))}>
       {children}
-    </SDiv>
+    </SHeader>
   );
 };
 
